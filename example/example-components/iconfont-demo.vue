88b601ce63cf2a6c5daccc2aef8c88bc8d704726
wmui <qq22337383@gmail.com>
2018-11-10 22:35:05 +0800

    [feat] add iconfont

--- example/example-components/iconfont-demo.vue
+++ example/example-components/iconfont-demo.vue
@@ -1,17 +1,11 @@
 <template>
-<!-- iconfont 复制区域 -->
+  <!-- iconfont 复制区域 -->
   <ul class="icon_lists clear">
 
     <li>
       <i class="icon wmui icon-round-close"></i>
       <div class="name">round_close</div>
       <div class="fontclass">.icon-round-close</div>
-    </li>
-
-    <li>
-      <i class="icon wmui icon-refresh"></i>
-      <div class="name">refresh</div>
-      <div class="fontclass">.icon-refresh</div>
     </li>
 
     <li>
@@ -51,9 +45,21 @@
     </li>
 
     <li>
+      <i class="icon wmui icon-image"></i>
+      <div class="name">image</div>
+      <div class="fontclass">.icon-image</div>
+    </li>
+
+    <li>
       <i class="icon wmui icon-upload"></i>
       <div class="name">upload</div>
       <div class="fontclass">.icon-upload</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-writefill"></i>
+      <div class="name">write_fill</div>
+      <div class="fontclass">.icon-writefill</div>
     </li>
 
     <li>
@@ -75,9 +81,69 @@
     </li>
 
     <li>
+      <i class="icon wmui icon-backtop"></i>
+      <div class="name">back-top</div>
+      <div class="fontclass">.icon-backtop</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-rss"></i>
+      <div class="name">rss</div>
+      <div class="fontclass">.icon-rss</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-drafts"></i>
+      <div class="name">drafts</div>
+      <div class="fontclass">.icon-drafts</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-weixin"></i>
+      <div class="name">weixin</div>
+      <div class="fontclass">.icon-weixin</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-setting"></i>
+      <div class="name">setting</div>
+      <div class="fontclass">.icon-setting</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-select"></i>
+      <div class="name">下拉</div>
+      <div class="fontclass">.icon-select</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-close"></i>
+      <div class="name">close</div>
+      <div class="fontclass">.icon-close</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-github"></i>
+      <div class="name">github</div>
+      <div class="fontclass">.icon-github</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-loading"></i>
+      <div class="name">loading</div>
+      <div class="fontclass">.icon-loading</div>
+    </li>
+
+    <li>
       <i class="icon wmui icon-query"></i>
       <div class="name">query</div>
       <div class="fontclass">.icon-query</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-comment"></i>
+      <div class="name">comment</div>
+      <div class="fontclass">.icon-comment</div>
     </li>
 
     <li>
@@ -105,13 +171,48 @@
     </li>
 
     <li>
+      <i class="icon wmui icon-twitter"></i>
+      <div class="name">twitter</div>
+      <div class="fontclass">.icon-twitter</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-sina"></i>
+      <div class="name">sina</div>
+      <div class="fontclass">.icon-sina</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-stack-overflow"></i>
+      <div class="name">stack-overflow</div>
+      <div class="fontclass">.icon-stack-overflow</div>
+    </li>
+
+    <li>
+      <i class="icon wmui icon-save"></i>
+      <div class="name">save</div>
+      <div class="fontclass">.icon-save</div>
+    </li>
+
+    <li>
       <i class="icon wmui icon-check"></i>
       <div class="name">check</div>
       <div class="fontclass">.icon-check</div>
     </li>
 
-  </ul>
+    <li>
+      <i class="icon wmui icon-menu"></i>
+      <div class="name">menu</div>
+      <div class="fontclass">.icon-menu</div>
+    </li>
 
+    <li>
+      <i class="icon wmui icon-logout"></i>
+      <div class="name">logout</div>
+      <div class="fontclass">.icon-logout</div>
+    </li>
+
+  </ul>
 </template>
 <style lang="scss">
 .icon_lists {
